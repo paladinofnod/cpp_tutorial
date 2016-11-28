215fde5f3cc60a1de7f0b3f68cb8b89f38af2440
paladinofnod <razielnod@mail.ru>
2016-11-28 14:42:11 +0500

    Добавлена ветка для разработки. Проведены изменения в основном коде (подготовлен каркас)
    Signed-off-by: paladinofnod <razielnod@mail.ru>

--- main.cpp
+++ main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+void vremya();
+void pi();
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Выберите программу: " << endl;
+    cout << "1 - Время" << endl;
+    cout << "2 - Число Пи" << endl;
+
+    int choise; // переменная для выбора запускаемой программы
+    cin >> choise;
+
     return 0;
 }
